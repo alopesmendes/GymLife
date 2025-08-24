41cfae0616a8806fcc397f279bf38e656012d177
alopesmendes <56479133+alopesmendes@users.noreply.github.com>
2025-08-24 09:26:13 +0200

    (:wrench:) feat[#7]: add ktlint and pre-commit configuration (#13)
    Co-authored-by: ailtonlopesmendes <ailtonlopesmendes@ippon.fr>

--- composeApp/src/desktopMain/kotlin/com/ailtontech/gymlife/main.kt
+++ composeApp/src/desktopMain/kotlin/com/ailtontech/gymlife/main.kt
@@ -3,11 +3,12 @@ package com.ailtontech.gymlife
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "GymLife",
-    ) {
-        App()
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "GymLife",
+        ) {
+            App()
+        }
     }
-}
